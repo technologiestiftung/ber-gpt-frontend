0ac892ec2d75561291ffd042ae138640facd8335
Raphael Arce <raphael.arce@ts.berlin>
2024-07-22 15:33:51 +0200

    chore: initial setup
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+import technologiestiftung from "@technologiestiftung/prettier-config";
+
+// eslint-disable-next-line @technologiestiftung/no-default-export
+export default {
+	...technologiestiftung,
+	plugins: ["prettier-plugin-tailwindcss"],
+};
