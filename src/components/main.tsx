a55b373cec84ac42c351aabd725d5bf4488fb6e6
Raphael Arce <raphael.arce@ts.berlin>
2024-07-23 17:09:30 +0200

    feat: add rough layout of the page with placeholders and a basic chat
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

---
+++ src/components/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Chat } from "./chat/chat";
+
+export const Main: React.FC = () => {
+	return (
+		<main className="flex flex-1 py-10">
+			<div className="flex w-full">
+				<aside className="w-72 border">Placeholder Chat-History</aside>
+				<section className="flex w-full items-center justify-center pl-10">
+					<Chat />
+				</section>
+			</div>
+		</main>
+	);
+};
