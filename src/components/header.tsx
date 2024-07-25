79a968be1a79b3dc4194aaf9ca55b9c5e445bfc8
aeschi <56318362+aeschi@users.noreply.github.com>
2024-07-25 09:51:30 +0200

    feat: adjust styling for header (#8)
    * feat: adjust styling for header
    
    * fix: bug for primary button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { ChatIcon } from "./icons/chat-icon";
+import { MailIcon } from "./icons/mail-icon";
+import { VermerkIcon } from "./icons/vermerk-icon";
+import { PDFIcon } from "./icons/pdf-icon";
 
 export const Header: React.FC = () => {
 	return (
@@ -8,21 +12,25 @@ export const Header: React.FC = () => {
 				src="https://logos.citylab-berlin.org/logo-berlin.svg"
 				alt={"Berlin Logo"}
 			/>
-			<div className="flex pt-10">
-				<div className="w-96">
-					<h1 className="text-2xl font-bold">BärGPT</h1>
-					<h2 className="text-2xl">KI Testumgebung</h2>
+			<div className="flex pt-5">
+				<div className="w-72">
+					<h1 className="text-[22px] font-bold">BärGPT</h1>
+					<h2 className="text-[22px]">KI Testumgebung</h2>
 				</div>
-				<nav className="flex w-full">
-					<ul className="flex w-full justify-between">
+				<nav className="flex w-full shrink pt-1">
+					<ul className="flex h-fit w-full justify-between px-10 text-[17px]">
 						{[
-							"Chatfunktion",
-							"E-Mail Funktion",
-							"Vermerk Funktion",
-							"Zusammenfassen",
+							{ label: "Chat Funktion", icon: <ChatIcon /> },
+							{ label: "E-Mail Funktion", icon: <MailIcon /> },
+							{ label: "Vermerk Funktion", icon: <VermerkIcon /> },
+							{ label: "Zusammenfassen", icon: <PDFIcon /> },
 						].map((item) => (
-							<li key={item}>
-								<a className="text-dark-grey">{item}</a>
+							<li
+								key={item.label}
+								className="flex flex-row gap-2 border-transparent hover:border-b-2 hover:border-dark-grey hover:font-semibold"
+							>
+								{item.icon}
+								<a className="text-dark-grey">{item.label}</a>
 							</li>
 						))}
 					</ul>
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,7 +3,7 @@ import { Chat } from "./chat/chat";
 
 export const Main: React.FC = () => {
 	return (
-		<main className="flex flex-1 py-10">
+		<main className="flex flex-1 py-5">
 			<div className="flex w-full">
 				<aside className="w-72 border">Placeholder Chat-History</aside>
 				<section className="flex w-full items-center justify-center pl-10">
