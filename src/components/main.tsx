1f969c5f2c5f7bd9e3935989ba360d7cebd031d7
aeschi <anna.eschenbacher@icloud.com>
2024-07-30 15:28:45 +0200

    feat: cleanup for merge

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { Chat } from "./chat/chat";
 import { HistoryBar } from "./history-bar/history-bar";
-import { Email } from "./email/email";
 
 export const Main: React.FC = () => {
-	const [showChat, setShowChat] = React.useState(true);
-
 	return (
 		<main className="flex flex-1 overflow-hidden py-6">
 			<div className="flex w-full">
 				<HistoryBar />
 				<section className="flex w-full items-center justify-center pl-10">
-					{showChat ? <Chat /> : <Email />}
-					<button className="bg-red-200" onClick={() => setShowChat(!showChat)}>
-						Toggle Chat/Mail
-					</button>
+					<Chat />
 				</section>
 			</div>
 		</main>
