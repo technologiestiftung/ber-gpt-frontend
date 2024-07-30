a0ec57ae7596366e41de3d14ea7bb9bb209fc15c
aeschi <anna.eschenbacher@icloud.com>
2024-07-30 13:59:14 +0200

    feat: add toggle for testing

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -4,13 +4,17 @@ import { HistoryBar } from "./history-bar/history-bar";
 import { Email } from "./email/email";
 
 export const Main: React.FC = () => {
+	const [showChat, setShowChat] = React.useState(true);
+
 	return (
 		<main className="flex flex-1 overflow-hidden py-6">
 			<div className="flex w-full">
 				<HistoryBar />
 				<section className="flex w-full items-center justify-center pl-10">
-					{/* <Chat /> */}
-					<Email />
+					{showChat ? <Chat /> : <Email />}
+					<button className="bg-red-200" onClick={() => setShowChat(!showChat)}>
+						Toggle Chat/Mail
+					</button>
 				</section>
 			</div>
 		</main>
