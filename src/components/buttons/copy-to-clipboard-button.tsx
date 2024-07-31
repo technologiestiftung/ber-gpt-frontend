4e7112bdbbbedb8120102ed5c9c4ab62db30d36a
aeschi <anna.eschenbacher@icloud.com>
2024-07-31 12:06:50 +0200

    feat: icons and copy functinality

---
+++ src/components/buttons/copy-to-clipboard-button.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { CopyIcon } from "../icons/copy-icon";
+import { CheckIcon } from "../icons/check-icon";
+
+export function CopyToClipboardButton({
+	generatedAnswer,
+}: {
+	generatedAnswer: string;
+}) {
+	const [isCopiedToClipboard, setIsCopiedToClipboard] = useState(false);
+
+	const copyToClipboard = async () => {
+		await navigator.clipboard.writeText(generatedAnswer);
+		setIsCopiedToClipboard(true);
+	};
+
+	/**
+	 * reset icon after 2 seconds
+	 */
+	useEffect(() => {
+		const timeoutId = setTimeout(() => setIsCopiedToClipboard(false), 2000);
+
+		return () => clearTimeout(timeoutId);
+	}, [isCopiedToClipboard]);
+
+	return (
+		<>
+			<button
+				className={`hover:text-mid-blue text-dark-blue`}
+				onClick={copyToClipboard}
+				aria-label={isCopiedToClipboard ? "Kopiert!" : "Kopieren"}
+				title={isCopiedToClipboard ? "Kopiert!" : "Kopieren"}
+			>
+				{isCopiedToClipboard ? <CheckIcon /> : <CopyIcon />}
+			</button>
+		</>
+	);
+}
--- src/components/chat/chat-messages/text-message.tsx
+++ src/components/chat/chat-messages/text-message.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import { BaerIcon } from "../../icons/bear-icon";
+import { RefreshIcon } from "../../icons/refresh-icon";
+import { CopyToClipboardButton } from "../../buttons/copy-to-clipboard-button";
 
 interface TextMessageProps {
 	role: string;
@@ -9,8 +12,21 @@ interface TextMessageProps {
 export const TextMessage: React.FC<TextMessageProps> = ({ role, content }) => {
 	return (
 		<div
-			className={`max-w-[60%] rounded border-2 p-4 shadow-md ${role === "user" ? "self-end border-mid-grey" : "self-start border-dark-blue"} `}
+			className={`max-w-[60%] rounded border-2 p-2 shadow-md ${role === "user" ? "self-end border-mid-grey" : "self-start border-dark-blue"} `}
 		>
+			<div
+				className={`flex flex-row items-center justify-between gap-2 ${role === "assistant" ? "" : "hidden"}`}
+			>
+				<div
+					className={`mb-2 flex min-h-[37px] w-[37px] items-center justify-center rounded-full bg-white drop-shadow-lg`}
+				>
+					<BaerIcon className="h-[21px] w-[21px]" />
+				</div>
+				<div className="flex flex-row items-center gap-3 self-start p-2 text-dark-blue">
+					<RefreshIcon />
+					<CopyToClipboardButton generatedAnswer={content} />
+				</div>
+			</div>
 			<ReactMarkdown className="markdown-container">
 				{content === "" ? "..." : content}
 			</ReactMarkdown>
