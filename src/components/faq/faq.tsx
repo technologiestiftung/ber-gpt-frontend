d15cac71b4f51276d5a1045920d28296725a4050
aeschi <anna.eschenbacher@icloud.com>
2024-08-02 11:13:10 +0200

    feat: refactoring logoLinks

--- src/components/faq/faq.tsx
+++ src/components/faq/faq.tsx
@@ -2,32 +2,9 @@ import React from "react";
 import { XIcon } from "../icons/x-icon";
 import { QuestionAnswer } from "./question-answer";
 import { qas } from "./qas";
+import { logoLinks } from "../logo-links";
 
 export const Faq: React.FC = () => {
-	const logoLinks = [
-		{
-			href: "https://citylab-berlin.org/de/start/",
-			alt: "Logo CityLAB Berlin",
-			src: "https://logos.citylab-berlin.org/logo-citylab-color.svg",
-			width: 131,
-			height: 28,
-		},
-		{
-			href: "https://technologiestiftung-berlin.de/",
-			alt: "Logo der Technologiestiftung Berlin",
-			src: "https://logos.citylab-berlin.org/logo-tsb-outline.svg",
-			width: 92,
-			height: 28,
-		},
-		{
-			href: "https://www.berlin.de/senatskanzlei/",
-			alt: "Logo des Regierenden Bürgermeisters von Berlin und der Senatskanzlei",
-			src: "https://logos.citylab-berlin.org/logo-senatskanzlei-buergermeister-horizontal.svg",
-			width: 168,
-			height: 28,
-		},
-	];
-
 	return (
 		<dialog
 			id="faq-dialog"
--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { useSplashStore } from "../store/splash-store";
 import { BaerIcon } from "./icons/bear-icon";
 import { XIcon } from "./icons/x-icon";
+import { logoLinks } from "./logo-links";
 
 export const SplashScreen: React.FC = () => {
 	const { hideSplashScreen } = useSplashStore();
@@ -38,77 +39,54 @@ export const SplashScreen: React.FC = () => {
 		{ href: "https://citylab-berlin.org/de/imprint/", text: "Impressum" },
 	];
 
-	const logoLinks = [
-		{
-			href: "https://citylab-berlin.org/de/start/",
-			alt: "Logo CityLAB Berlin",
-			src: "https://logos.citylab-berlin.org/logo-citylab-color.svg",
-			width: 131,
-			height: 28,
-		},
-		{
-			href: "https://technologiestiftung-berlin.de/",
-			alt: "Logo der Technologiestiftung Berlin",
-			src: "https://logos.citylab-berlin.org/logo-tsb-outline.svg",
-			width: 92,
-			height: 28,
-		},
-		{
-			href: "https://www.berlin.de/senatskanzlei/",
-			alt: "Logo des Regierenden Bürgermeisters von Berlin und der Senatskanzlei",
-			src: "https://logos.citylab-berlin.org/logo-senatskanzlei-buergermeister-horizontal.svg",
-			width: 168,
-			height: 28,
-		},
-	];
-
 	return (
 		<div
 			ref={splashContainer}
 			className={`pointer-events-auto m-1 flex h-fit w-fit rounded border-2 border-mid-grey bg-white shadow-lg sm:mx-auto sm:my-10 sm:h-fit sm:w-[510px]`}
 		>
-			<div className="relative flex flex-col gap-6 rounded p-8"></div>
-			<div className="flex min-h-[63px] w-[63px] items-center justify-center rounded-full bg-white drop-shadow-lg">
-				<BaerIcon className="h-[40px] w-[40px]" />
-			</div>
-			<div>
-				<h1 className="text-[22px] font-bold leading-6">BärGPT</h1>
-				<h2 className="text-[22px]">KI Testumgebung</h2>
-			</div>
-			<p className="py-2">
-				Der Einsatz von <b>BärGPT</b> in der öffentlichen Verwaltung bietet eine
-				Vielzahl von Vorteilen für Mitarbeitende. Es kann Routineanfragen
-				effizient und rund um die Uhr beantworten, wodurch Verwaltungspersonal
-				entlastet und der Service für Bürgerinnen und Bürger verbessert wird.
-				Mitarbeitende können sich so auf komplexere und wertschöpfendere
-				Aufgaben konzentrieren. Darüber hinaus tragen Chatbots zur
-				Beschleunigung von Verwaltungsprozessen bei und erhöhen die
-				Zufriedenheit der Bürger:innen durch schnellere und präzisere Auskünfte.
-				Letztlich fördert BärGPT eine moderne, digitale Berliner Verwaltung und
-				verbessert die Effizienz sowie Servicequalität.
-			</p>
-			<div className="flex flex-row flex-wrap justify-between gap-2">
-				{links.map((link) => (
-					<a
-						key={link.text}
-						href={link.href}
-						className="text-mid-blue underline hover:text-dark-blue"
-					>
-						{link.text}
-					</a>
-				))}
-			</div>
-			<div className="flex flex-row flex-wrap justify-between gap-6">
-				{logoLinks.map((logo) => (
-					<a key={logo.alt} href={logo.href} target="_blank" rel="noreferrer">
-						<img
-							alt={logo.alt}
-							src={logo.src}
-							width={logo.width}
-							height={logo.height}
-						/>
-					</a>
-				))}
+			<div className="relative flex flex-col gap-6 rounded p-8">
+				<div className="flex min-h-[63px] w-[63px] items-center justify-center rounded-full bg-white drop-shadow-lg">
+					<BaerIcon className="h-[40px] w-[40px]" />
+				</div>
+				<div>
+					<h1 className="text-[22px] font-bold leading-6">BärGPT</h1>
+					<h2 className="text-[22px]">KI Testumgebung</h2>
+				</div>
+				<p className="py-2">
+					Der Einsatz von <b>BärGPT</b> in der öffentlichen Verwaltung bietet
+					eine Vielzahl von Vorteilen für Mitarbeitende. Es kann Routineanfragen
+					effizient und rund um die Uhr beantworten, wodurch Verwaltungspersonal
+					entlastet und der Service für Bürgerinnen und Bürger verbessert wird.
+					Mitarbeitende können sich so auf komplexere und wertschöpfendere
+					Aufgaben konzentrieren. Darüber hinaus tragen Chatbots zur
+					Beschleunigung von Verwaltungsprozessen bei und erhöhen die
+					Zufriedenheit der Bürger:innen durch schnellere und präzisere
+					Auskünfte. Letztlich fördert BärGPT eine moderne, digitale Berliner
+					Verwaltung und verbessert die Effizienz sowie Servicequalität.
+				</p>
+				<div className="flex flex-row flex-wrap justify-between gap-2">
+					{links.map((link) => (
+						<a
+							key={link.text}
+							href={link.href}
+							className="text-mid-blue underline hover:text-dark-blue"
+						>
+							{link.text}
+						</a>
+					))}
+				</div>
+				<div className="flex flex-row flex-wrap justify-between gap-6">
+					{logoLinks.map((logo) => (
+						<a key={logo.alt} href={logo.href} target="_blank" rel="noreferrer">
+							<img
+								alt={logo.alt}
+								src={logo.src}
+								width={logo.width}
+								height={logo.height}
+							/>
+						</a>
+					))}
+				</div>
 			</div>
 
 			<button
