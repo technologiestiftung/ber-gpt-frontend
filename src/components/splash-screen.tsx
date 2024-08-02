0de30878ed297b7197bbe1785fd291aea4dba5cd
aeschi <anna.eschenbacher@icloud.com>
2024-08-02 11:00:09 +0200

    feat: add array with logo links and links

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -28,12 +28,43 @@ export const SplashScreen: React.FC = () => {
 		hideSplashScreen();
 	};
 
+	const links = [
+		{ href: "https://citylab-berlin.org/de/start/", text: "Feedback" },
+		{ href: "https://citylab-berlin.org/de/start/", text: "Kontakt" },
+		{ href: "https://citylab-berlin.org/de/data-privacy/", text: "Datenschutz" },
+		{ href: "https://citylab-berlin.org/de/imprint/", text: "Impressum" },
+	];
+
+	const logoLinks = [
+		{
+			href: "https://citylab-berlin.org/de/start/",
+			alt: "Logo CityLAB Berlin",
+			src: "https://logos.citylab-berlin.org/logo-citylab-color.svg",
+			width: 131,
+			height: 28,
+		},
+		{
+			href: "https://technologiestiftung-berlin.de/",
+			alt: "Logo der Technologiestiftung Berlin",
+			src: "https://logos.citylab-berlin.org/logo-tsb-outline.svg",
+			width: 92,
+			height: 28,
+		},
+		{
+			href: "https://www.berlin.de/senatskanzlei/",
+			alt: "Logo des Regierenden Bürgermeisters von Berlin und der Senatskanzlei",
+			src: "https://logos.citylab-berlin.org/logo-senatskanzlei-buergermeister-horizontal.svg",
+			width: 168,
+			height: 28,
+		},
+	];
+
 	return (
 		<div
 			ref={splashContainer}
 			className={`pointer-events-auto m-1 flex h-fit w-fit rounded border-2 border-mid-grey bg-white shadow-lg sm:mx-auto sm:my-10 sm:h-fit sm:w-[510px]`}
 		>
-			<div className="relative flex flex-col gap-6 rounded p-8">
+			<div className="relative flex flex-col gap-6 rounded p-8"></div>
 				<div className="flex min-h-[63px] w-[63px] items-center justify-center rounded-full bg-white drop-shadow-lg">
 					<BaerIcon className="h-[40px] w-[40px]" />
 				</div>
@@ -54,68 +85,32 @@ export const SplashScreen: React.FC = () => {
 					Verwaltung und verbessert die Effizienz sowie Servicequalität.
 				</p>
 				<div className="flex flex-row flex-wrap justify-between gap-2">
-					<a
-						href="https://citylab-berlin.org/de/start/"
-						className="text-mid-blue underline hover:text-dark-blue"
-					>
-						Feedback
-					</a>
-					<a
-						href="https://citylab-berlin.org/de/start/"
-						className="text-mid-blue underline hover:text-dark-blue"
-					>
-						Kontakt
-					</a>
-					<a
-						href="https://citylab-berlin.org/de/data-privacy/"
-						className="text-mid-blue underline hover:text-dark-blue"
-					>
-						Datenschutz
-					</a>
-					<a
-						href="https://citylab-berlin.org/de/imprint/"
-						className="text-mid-blue underline hover:text-dark-blue"
-					>
-						Impressum
-					</a>
+					{links.map((link) => (
+						<a
+							key={link.text}
+							href={link.href}
+							className="text-mid-blue underline hover:text-dark-blue"
+						>
+							{link.text}
+						</a>
+					))}
 				</div>
 				<div className="flex flex-row flex-wrap justify-between gap-6">
-					<a
-						href="https://citylab-berlin.org/de/start/"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<img
-							alt="Logo CityLAB Berlin"
-							src="https://logos.citylab-berlin.org/logo-citylab-color.svg"
-							width={131}
-							height={28}
-						/>
-					</a>
-					<a
-						href="https://technologiestiftung-berlin.de/"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<img
-							alt="Logo der Technologiestiftung Berlin"
-							src="https://logos.citylab-berlin.org/logo-tsb-outline.svg"
-							width={92}
-							height={28}
-						/>
-					</a>
-					<a
-						href="https://www.berlin.de/senatskanzlei/"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<img
-							alt="Logo des Regierenden Bürgermeisters von Berlin und der Senatskanzlei"
-							src="https://logos.citylab-berlin.org/logo-senatskanzlei-buergermeister-horizontal.svg"
-							width={168}
-							height={28}
-						/>
-					</a>
+					{logoLinks.map((logo) => (
+						<a
+							key={logo.alt}
+							href={logo.href}
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								alt={logo.alt}
+								src={logo.src}
+								width={logo.width}
+								height={logo.height}
+							/>
+						</a>
+					))}
 				</div>
 
 				<button
