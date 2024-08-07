a9bfa8fca5929ec799d06a47bd342f47bbcf2259
Raphael Arce <raphael.arce@ts.berlin>
2024-08-07 16:39:18 +0200

    chore: remove prettier tailwind plugin
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,6 @@
 import technologiestiftung from "@technologiestiftung/prettier-config";
-import { fileURLToPath } from "node:url";
-import { dirname, resolve } from "node:path";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // eslint-disable-next-line @technologiestiftung/no-default-export
 export default {
 	...technologiestiftung,
-	plugins: ["prettier-plugin-tailwindcss"],
-	tailwindConfig: resolve(__dirname, "./tailwind.config.js"),
 };
