f6bbc9ec477eb9d269d5baf58389c781c696f7bf
aeschi <anna.eschenbacher@icloud.com>
2024-08-07 17:26:09 +0200

    fix: prettier issues

--- src/components/faq/qas.tsx
+++ src/components/faq/qas.tsx
@@ -5,8 +5,7 @@ export const qas = [
 			"BärGPT ist eine spezialisierte Chat-Anwendung, die auf die Anforderungen der Berliner Verwaltung zugeschnitten ist. Es dient als intelligenter Assistent, der Verwaltungsmitarbeitenden bei täglichen Aufgaben unterstützt, indem es fortschrittliche KI-Technologien nutzt. Neben der Chatfunktion bietet BärGPT eine Reihe von Mini KI Anwendungen (KI Apps), die spezifische Aufgaben wie das Schreiben von Vermerken, das Formulieren von E-Mails und das Zusammenfassen von Texten erleichtern.",
 	},
 	{
-		question:
-			"Bei welche Aufgaben kann BärGPT unterstützen?",
+		question: "Bei welche Aufgaben kann BärGPT unterstützen?",
 		answer:
 			"BärGPT kann eine Vielzahl von Aufgaben übernehmen. Es bietet einen einfachen und inuitiven Zugang zu den vielseitigen Fähigkeiten eines Large Language Modells. So kann BärGPT bei der Erstellung formeller Vermerke unterstützen, basierend auf bereitgestellten Informationen. Auch das Formulieren und Optimieren von E-Mails für verschiedene administrative Zwecke gehört zu den Funktionen. Zudem ist BärGPT in der Lage, lange Dokumente oder Textpassagen zusammenzufassen, um eine schnellere Informationsaufnahme zu ermöglichen. Diese Funktionen sollen den Arbeitsalltag in der Berliner Verwaltung effizienter gestalten.",
 	},
@@ -16,8 +15,7 @@ export const qas = [
 			'BärGPT nutzt ein Large Language Model, um verschiedene Textaufgaben zu bewältigen. Es analysiert eingehende Anfragen und generiert darauf basierend passende Antworten oder Texte. Für spezifische Aufgaben stehen spezielle KI Apps zur Verfügung, die über ein kontinuierlich erweitertes App-Store-Interface zugänglich sind. Derzeit befindet sich BärGPT als "Proof of Concept" im Testbetrieb, um seine Einsatzmöglichkeiten in der Verwaltung zu evaluieren.',
 	},
 	{
-		question:
-			"Was ist ein Large Language Model?",
+		question: "Was ist ein Large Language Model?",
 		answer:
 			'Ein "Large Language Model" (LLM) ist ein hochentwickeltes KI-gestütztes System, das speziell dafür konzipiert ist, menschliche Sprache in ihrer Komplexität zu erfassen und zu simulieren. Diese Technologie basiert auf der Analyse und Verarbeitung einer immensen Menge an Textdaten, die aus einer Vielzahl von Quellen wie Büchern, Artikeln und Internetinhalten stammen. Durch maschinelles Lernen und komplexe Algorithmen entwickelt das Modell die Fähigkeit, Texte zu verstehen, zu generieren und in menschenähnlicher Weise zu interagieren. Es kann Aufgaben wie das Beantworten von Fragen, das Übersetzen von Sprachen und das Erstellen von Texten verschiedenster Art übernehmen. Trotz ihrer beeindruckenden Leistungsfähigkeit sind LLMs nicht fehlerfrei und ihre Interpretationen können gelegentlich von menschlichem Verständnis abweichen. Sie repräsentieren den aktuellen Stand der KI-Entwicklung im Bereich der natürlichen Sprachverarbeitung, aber sie bleiben ein Werkzeug, dessen Ergebnisse stets kritisch hinterfragt und kontextualisiert werden sollten.',
 	},
@@ -30,16 +28,16 @@ export const qas = [
 	{
 		question: "Wer hat BärGPT entwickelt?",
 		answer:
-			'BärGPT ist ein Prototyp des CityLAB Berlin. Das CityLAB ist Berlins öffentliches Innovationslabor. An der Schnittstelle von Zivilgesellschaft, Verwaltung, Wissenschaft und Wirtschaft nutzen wir die Potenziale der Digitalisierung für eine gemeinwohlorientierte Stadtentwicklung – praxisnah und wirkungsorientiert. Das CityLAB Berlin ist ein Projekt der Technologiestiftung Berlin und gefördert durch die Berliner Senatskanzlei.',
+			"BärGPT ist ein Prototyp des CityLAB Berlin. Das CityLAB ist Berlins öffentliches Innovationslabor. An der Schnittstelle von Zivilgesellschaft, Verwaltung, Wissenschaft und Wirtschaft nutzen wir die Potenziale der Digitalisierung für eine gemeinwohlorientierte Stadtentwicklung – praxisnah und wirkungsorientiert. Das CityLAB Berlin ist ein Projekt der Technologiestiftung Berlin und gefördert durch die Berliner Senatskanzlei.",
 	},
 	{
 		question: "Kann BärGPT auch für andere Anwendungsfälle genutzt werden?",
 		answer:
-			'BärGPT ist ein flexibler Prototyp und kann prinzipiell für verschiedene Anwendungsfälle und Kontexte angepasst und weiterentwickelt werden. Die zugrunde liegende Technologie kann für die Analyse und Verarbeitung von Textdaten in unterschiedlichen Bereichen eingesetzt werden. Interessierte Organisationen oder Entwickler können auf Anfrage weitere Informationen zur Anpassung und Integration von BärGPT erhalten.',
+			"BärGPT ist ein flexibler Prototyp und kann prinzipiell für verschiedene Anwendungsfälle und Kontexte angepasst und weiterentwickelt werden. Die zugrunde liegende Technologie kann für die Analyse und Verarbeitung von Textdaten in unterschiedlichen Bereichen eingesetzt werden. Interessierte Organisationen oder Entwickler können auf Anfrage weitere Informationen zur Anpassung und Integration von BärGPT erhalten.",
 	},
 	{
 		question: "Datenschutz und Umgang mit Drittanbietern",
 		answer:
-			'BärGPT nutzt in der Entwicklungsphase ein kommerzielles Sprachmodell von OpenAI. Die Server, auf denen diese Dienstleistung betrieben wird, befinden sich in den Vereinigten Staaten. Im Hinblick auf den Datenschutz, insbesondere unter Berücksichtigung der europäischen Datenschutzverordnungen, raten wir davon ab, persönliche Informationen in Suchanfragen einzugeben. Es ist wichtig, sich der geografischen Lokalisierung der Server und der damit verbundenen Datenschutzbestimmungen bewusst zu sein. Für den späteren Regelbetrieb von BärGPT ist der Einsatz eines datenschutzkonformen Sprachmodells mit Serverstandort in Europa vorgesehen.',
+			"BärGPT nutzt in der Entwicklungsphase ein kommerzielles Sprachmodell von OpenAI. Die Server, auf denen diese Dienstleistung betrieben wird, befinden sich in den Vereinigten Staaten. Im Hinblick auf den Datenschutz, insbesondere unter Berücksichtigung der europäischen Datenschutzverordnungen, raten wir davon ab, persönliche Informationen in Suchanfragen einzugeben. Es ist wichtig, sich der geografischen Lokalisierung der Server und der damit verbundenen Datenschutzbestimmungen bewusst zu sein. Für den späteren Regelbetrieb von BärGPT ist der Einsatz eines datenschutzkonformen Sprachmodells mit Serverstandort in Europa vorgesehen.",
 	},
 ];
