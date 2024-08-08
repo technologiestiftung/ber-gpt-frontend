16b591ba903b72ef14de2f95321c257598330b83
aeschi <anna.eschenbacher@icloud.com>
2024-08-08 16:28:31 +0200

    feat: add a href link to home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,13 @@ import React from "react";
 export const Header: React.FC = () => {
 	return (
 		<header className="flex items-start z-40 bg-white px-5 py-2 border-b border-darker-grey">
-			<img
-				className="w-20 sm:pl-0 md:w-32"
-				src="https://logos.citylab-berlin.org/logo-berlin.svg"
-				alt="Berlin Logo"
-			/>
+			<a href="/">
+				<img
+					className="w-20 sm:pl-0 md:w-32"
+					src="https://logos.citylab-berlin.org/logo-berlin.svg"
+					alt="Berlin Logo"
+				/>
+			</a>
 		</header>
 	);
 };
