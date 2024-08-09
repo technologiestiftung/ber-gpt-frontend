8f7701fee7de9c15f43d0780126de28354e13a5e
aeschi <anna.eschenbacher@icloud.com>
2024-08-09 14:10:38 +0200

    feat: adjust text color to navigation style

--- src/components/buttons/chat-box-button.tsx
+++ src/components/buttons/chat-box-button.tsx
@@ -13,7 +13,7 @@ export const ChatBoxButton: React.FC<ButtonProps> = ({
 }) => {
 	return (
 		<button
-			className={`text-md min-h-[135px]  my-1.5 flex h-fit w-fit items-start justify-center rounded-sm  px-3 py-4 font-normal text-dark-blue  disabled:bg-light-grey disabled:font-normal disabled:text-mid-grey ${
+			className={`text-md min-h-[135px]  my-1.5 flex h-fit w-fit items-start justify-center rounded-sm  px-3 py-4 font-normal text-darker-grey  disabled:bg-light-grey disabled:font-normal disabled:text-mid-grey ${
 				className
 			}`}
 			disabled={disabled}
