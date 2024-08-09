dac7d3d1c5082ba5e35617e54630920b693bc7de
aeschi <anna.eschenbacher@icloud.com>
2024-08-09 11:03:35 +0200

    feat: add sidebar container for darker backdrop

--- src/components/sidebar-bar/side-bar.tsx
+++ src/components/sidebar-bar/side-bar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { IconButton } from "../buttons/icon-button";
 import { SidebarIcon } from "../icons/sidebar-icon";
 import { NewChatIcon } from "../icons/new-chat-icon";
@@ -7,6 +7,8 @@ import { FaqIcon } from "../icons/faq-icon";
 import { Navigation } from "./navigation";
 import { History } from "./history/history";
 
+const mdWidth = 768;
+
 export const SideBar: React.FC = () => {
 	const [isOpen, setIsOpen] = useState(true);
 
@@ -14,7 +16,7 @@ export const SideBar: React.FC = () => {
 
 	useEffect(() => {
 		// close sidebar on mobile when a chat is selected
-		if (window.innerWidth < 768) {
+		if (window.innerWidth < mdWidth) {
 			setIsOpen(false);
 		}
 	}, [currentChatId]);
@@ -23,60 +25,88 @@ export const SideBar: React.FC = () => {
 		? "Seitenleiste schließen"
 		: "Seitenleiste öffnen";
 
+	const sidebarRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		document.addEventListener("mousedown", handleClickListener);
+
+		return () => {
+			document.removeEventListener("mousedown", handleClickListener);
+		};
+	}, []);
+
+	const handleClickListener = (event: MouseEvent) => {
+		if (
+			window.innerWidth < mdWidth &&
+			!sidebarRef?.current?.contains(event.target as Node)
+		) {
+			setIsOpen(false);
+		}
+	};
+
 	return (
-		<aside
-			className={`absolute md:relative flex pt-12 md:pt-0 top-0 flex-col left-0 z-30 justify-between overflow-hidden gap-2 overflow-x-hidden ${
+		<div
+			className={`absolute md:relative top-0 pt-11 md:pt-0 left-0 z-20 ${
 				isOpen
-					? "bg-sidebar-grey min-w-72 w-72 h-full pb-4"
-					: "min-w-24 w-28 h-fit"
+					? "w-full h-full backdrop-brightness-90 mh:min-w-72 md:w-72 md:backdrop-brightness-100"
+					: ""
 			}`}
-			aria-label="Sidebar"
 		>
-			<div className={`flex justify-between px-5 flex-row gap-2`}>
-				<IconButton
-					isOutlineVisible={!isOpen}
-					icon={<SidebarIcon />}
-					ariaLabel={toggleIsSidebarOpenLabel}
-					title={toggleIsSidebarOpenLabel}
-					onClick={() => setIsOpen(!isOpen)}
-				/>
-				<IconButton
-					isOutlineVisible={!isOpen}
-					icon={<NewChatIcon />}
-					ariaLabel="Neuen Chat starten"
-					title="Neuen Chat starten"
-					onClick={() => setCurrentChatId(null)}
-				/>
-			</div>
-			<div
-				className={`flex-col gap-8 pb-8 h-full overflow-y-auto ${isOpen ? "flex" : "hidden"}`}
+			<aside
+				ref={sidebarRef}
+				className={`flex flex-col z-30 justify-between overflow-hidden gap-2 overflow-x-hidden ${
+					isOpen
+						? "bg-sidebar-grey min-w-72 w-72 h-full pb-4"
+						: "min-w-24 w-28 h-fit"
+				}`}
+				aria-label="Sidebar"
 			>
+				<div className={`flex justify-between px-5 pt-2 flex-row gap-2`}>
+					<IconButton
+						isOutlineVisible={!isOpen}
+						icon={<SidebarIcon />}
+						ariaLabel={toggleIsSidebarOpenLabel}
+						title={toggleIsSidebarOpenLabel}
+						onClick={() => setIsOpen(!isOpen)}
+					/>
+					<IconButton
+						isOutlineVisible={!isOpen}
+						icon={<NewChatIcon />}
+						ariaLabel="Neuen Chat starten"
+						title="Neuen Chat starten"
+						onClick={() => setCurrentChatId(null)}
+					/>
+				</div>
 				<div
-					className={`flex-col px-5 text-[22px] ${isOpen ? "flex" : "hidden"}`}
+					className={`flex-col gap-8 pb-8 h-full overflow-y-auto ${isOpen ? "flex" : "hidden"}`}
 				>
-					<h1 className="font-bold">BärGPT</h1>
-					<h2>KI-Testumgebung</h2>
+					<div
+						className={`flex-col px-5 text-[22px] ${isOpen ? "flex" : "hidden"}`}
+					>
+						<h1 className="font-bold">BärGPT</h1>
+						<h2>KI-Testumgebung</h2>
+					</div>
+
+					<Navigation />
+
+					<History />
+
+					<div className={`px-5 text-sm text-dark-blue`}>
+						Der Chat Verlauf wird lokal gespeichert und ist somit nicht für
+						andere Personen sichtbar.
+					</div>
 				</div>
-
-				<Navigation />
-
-				<History />
-
-				<div className={`px-5 text-sm text-dark-blue`}>
-					Der Chat Verlauf wird lokal gespeichert und ist somit nicht für andere
-					Personen sichtbar.
-				</div>
-			</div>
-			<button
-				className={`px-5 w-fit text-dark-blue hover:text-light-blue ${isOpen ? "flex" : "hidden"}`}
-				onClick={() =>
-					(
-						document.getElementById("faq-dialog") as HTMLDialogElement
-					).showModal()
-				}
-			>
-				<FaqIcon className="h-6 w-6" />
-			</button>
-		</aside>
+				<button
+					className={`px-5 w-fit text-dark-blue hover:text-light-blue ${isOpen ? "flex" : "hidden"}`}
+					onClick={() =>
+						(
+							document.getElementById("faq-dialog") as HTMLDialogElement
+						).showModal()
+					}
+				>
+					<FaqIcon className="h-6 w-6" />
+				</button>
+			</aside>
+		</div>
 	);
 };
