729ab006ca9b3bfdb61531af2f63bd30bad22162
aeschi <anna.eschenbacher@icloud.com>
2024-08-09 14:13:49 +0200

    feat: add home link to title and switch berlin link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Header: React.FC = () => {
 	return (
 		<header className="flex items-start z-40 bg-white px-5 py-1.5 border-b border-mid-grey">
-			<a href="/">
+			<a href="https://www.berlin.de/" target="_blank" rel="noreferrer">
 				<img
 					className="sm:pl-0 min-w-[100px] w-[100px]"
 					src="https://logos.citylab-berlin.org/logo-berlin.svg"
