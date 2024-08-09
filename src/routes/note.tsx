d86504cf51920b47178d3864a57d9a6d97858ed0
aeschi <56318362+aeschi@users.noreply.github.com>
2024-08-09 13:33:49 +0200

    feat: add styled preview for note and summary (#37)

--- src/routes/note.tsx
+++ src/routes/note.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { Layout } from "../layout/layout";
+import { VermerkIcon } from "../components/icons/vermerk-icon";
 
 export const Note: React.FC = () => {
 	return (
 		<Layout>
-			<div className="flex h-full w-full flex-col items-center justify-center">
-				<h1 className="text-4xl font-bold">Bald verfügbar: Vermerk Hilfe</h1>
+			<div className="flex h-full w-full items-center justify-center">
+				<div className=" md:w-[640px] px-5 text-center items-center flex flex-col gap-6">
+					<div
+						className={`flex flex-row gap-3 w-fit rounded-sm px-4 py-2.5 bg-ber-yellow`}
+					>
+						<VermerkIcon /> Vermerk erstellen
+					</div>
+					<h1 className="text-5xl font-bold">Bald verfügbar</h1>
+					<div className="text-xl ">
+						Der Vermerk-Assistent unterstützt Sie, aussagekräftige und leicht
+						verständliche Sachverhalte, Entscheidungen und Hinweise zu
+						formulieren.
+					</div>
+				</div>
 			</div>
 		</Layout>
 	);
--- src/routes/summary.tsx
+++ src/routes/summary.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Layout } from "../layout/layout";
+import { PDFIcon } from "../components/icons/pdf-icon";
 
 export const Summary: React.FC = () => {
 	return (
 		<Layout>
-			<div className="flex h-full w-full flex-col items-center justify-center">
-				<h1 className="text-4xl font-bold">
-					Bald verfügbar: Dokumenten Zusammenfassung Hilfe
-				</h1>
+			<div className="flex h-full w-full items-center justify-center">
+				<div className=" md:w-[640px] px-5 text-center items-center flex flex-col gap-6">
+					<div
+						className={`flex flex-row gap-3 w-fit rounded-sm px-4 py-2.5 bg-ber-pink`}
+					>
+						<PDFIcon /> Text zusammenfassen
+					</div>
+					<h1 className="text-5xl font-bold">Bald verfügbar</h1>
+					<div className="text-xl ">
+						Mit der Funktion &quot;Zusammenfassen&quot; können Sie längere Text
+						einfach auf das Wesentliche reduzieren. So schaffen Sie sich einen
+						schnellen Überblick über die Inhalte oder geben Kolleg:innen einen
+						schnellen Einblick in das Dokument, welches Sie z.B. per E-Mail
+						versenden.
+					</div>
+				</div>
 			</div>
 		</Layout>
 	);
