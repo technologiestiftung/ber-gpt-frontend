c04ebd91a5660070d69925302005ceb4be1a0002
aeschi <anna.eschenbacher@icloud.com>
2024-08-26 13:30:59 +0200

    feat: always show getting started message

--- src/components/email-chat/email-chat.tsx
+++ src/components/email-chat/email-chat.tsx
@@ -1,19 +1,10 @@
 import React from "react";
-import { useCurrentChatIdStore } from "../../store/current-chat-id-store";
 import { EmailChatForm } from "./email-chat-form";
-import { GetStartedEmailChat } from "./get-started-email-chat";
 import { EmailChatMessages } from "./email-chat-messages";
-import { useChatHistoryStore } from "../../store/chat-history-store";
 
 export const EmailChat: React.FC = () => {
-	const { getChat } = useChatHistoryStore();
-	const { currentChatId } = useCurrentChatIdStore();
-	const messages = getChat(currentChatId)?.messages || [];
-
 	return (
 		<div className="flex h-full flex-col items-center justify-between pt-16 pb-5 relative">
-			{messages.length === 0 && <GetStartedEmailChat />}
-
 			<EmailChatMessages />
 
 			<EmailChatForm />
