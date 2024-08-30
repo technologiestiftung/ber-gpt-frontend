d0698657f4eb6cf599e102029c771d696e9028cd
aeschi <56318362+aeschi@users.noreply.github.com>
2024-08-30 12:16:55 +0200

    feat: adjust text size spacing and color (#68)

--- src/routes/note.tsx
+++ src/routes/note.tsx
@@ -6,16 +6,16 @@ export const Note: React.FC = () => {
 	return (
 		<Layout>
 			<div className="flex h-full w-full items-center justify-center">
-				<div className=" md:w-[640px] lg:w-[768px] px-5 text-center items-center flex flex-col gap-2">
+				<div className=" sm:w-[575px] px-5 text-center items-center flex flex-col gap-2">
 					<div
-						className={`flex flex-row gap-3 w-fit text-xs border border-black rounded-full px-3 py-1 `}
+						className={`flex flex-row gap-3 w-fit text-xs border border-ber-darker-grey rounded-full px-3 py-1 `}
 					>
 						<InfoIcon /> Bald verfügbar
 					</div>
-					<div className="text-3xl md:text-5xl font-bold text-ber-darker-grey pb-4">
+					<div className="text-3xl font-bold text-ber-darker-grey">
 						Vermerk erstellen
 					</div>
-					<div className="md:text-xl text-ber-darker-grey">
+					<div className="text-md text-ber-darker-grey">
 						Der Vermerk-Assistent unterstützt Sie, aussagekräftige und leicht
 						verständliche Sachverhalte, Entscheidungen und Hinweise zu
 						formulieren.
